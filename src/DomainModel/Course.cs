c9a9325f631908986430b5e05f7ce053e2f8a30a
Paulo Walraven <paulomvwalraven@gmail.com>
2022-03-07 19:37:50 -0300

    create DomainModel

---
+++ src/DomainModel/Course.cs
@@ -0,0 +1,14 @@
+namespace DomainModel;
+
+public class Course : Entity
+{
+    public string Name { get; }
+    public int Credits { get; }
+
+    public Course(long id, string name, int credits)
+    {
+        Id = id;
+        Name = name;
+        Credits = credits;
+    }
+}
